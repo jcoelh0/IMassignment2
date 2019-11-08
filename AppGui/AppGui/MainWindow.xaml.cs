508761e9b1a0453ed2af266e89325e8b1b839c86
jcoelh0 <jcoelho@ua.pt>
2019-11-08 11:07:10 +0000

    working with keyword

--- AppGui/AppGui/MainWindow.xaml.cs
+++ AppGui/AppGui/MainWindow.xaml.cs
@@ -23,21 +23,41 @@ namespace AppGui
     {
         private MmiCommunication mmiC;
         private GoogleSearchEngineUsingChrome sel;
+        private static bool orderDone = false;
+        private static bool orderStart = false;
+        public TimeSpan MyDefaultTimeout { get; private set; }
+        private static Tts t;
+        private static ChromeDriver driver;
 
         public MainWindow()
         {
-            //InitializeComponent();
-            Tts t = new Tts();
+            driver = new ChromeDriver();
+            //t = new Tts();
 
-            t.Speak("olá, tudo");
+            //t.Speak("2");
 
-            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+
+            openUberEatsChrome(driver);
+
+            mmiC = new MmiCommunication("localhost", 8000, "User1", "GUI");
             mmiC.Message += MmiC_Message;
-
+            //t.Speak("3");
             mmiC.Start();
 
-            //sel = new GoogleSearchEngineUsingChrome();
-            //sel.Shoud_Search_Using_Chrome();
+
+            //t.Speak("4");
+            Console.WriteLine("before driver...");
+
+            Console.WriteLine("after driver function...");
+
+            //t.Speak("coccocosocoasodasdas");
+            //t.Speak("5");
+
+            //there is something wrogn with the t.Speak
+            //maybe assync
+
+
+            //t.Speak("olá, tudo");
 
 
         }
@@ -64,9 +84,9 @@ namespace AppGui
             //string date = doc.Root.Element(xmlns + "emma:interpretation").Attribute("emma:confidence").Value;
 
 
-            var node = doc.XPathSelectElement("/mmi:mmi/mmi:startRequest/mmi:contentURL/mmi:data/emma:emma/emma:interpretation").Attribute("emma:confidence");
+            //var node = doc.XPathSelectElement("/mmi:mmi/mmi:startRequest/mmi:contentURL/mmi:data/emma:emma/emma:interpretation").Attribute("emma:confidence");
 
-            Console.WriteLine(node);
+            //Console.WriteLine(node);
 
             //var price = doc.Element("emma:confidence");
             //XNamespace s = "http://www.w3.org/2003/04/emma";
@@ -76,57 +96,153 @@ namespace AppGui
 
 
 
-            Console.WriteLine(json.recognized[0].ToString());
+            Console.WriteLine(json.recognized[2].ToString());
 
 
 
             //decimal confidence = decimal.Parse(body.Value);
 
-            Shape _s = null;
 
             //Console.WriteLine(body);
             //Console.WriteLine("helllooooo confidence: " + price);
+            //t.Speak("boaaaaaaaaas.");
 
+            //orderStart = true;
 
-
-            while (!((string)json.recognized[0].ToString() == "KEY" )){ //&& confidence > (decimal)0.75)) {
-                
+            
+            if((string)json.recognized[0].ToString() == "KEY") //&& confidence > (decimal)0.75)) {
+            {
+                orderStart = true;
+                //t.Speak("Olá! O que gostaria de fazer?");
+            }
+            else if((string)json.recognized[0].ToString() != "EXIT")
+            {
+                orderDone = true;
+                //t.Speak("ok, até breve!");
             }
 
-            //Tts t = new Tts();
+            if (orderStart)
+            {
 
+                //var searchBox = driver.FindElement(By.CssSelector("#search-suggestions-input"));
+                switch ((string)json.recognized[1].ToString())
+                {
+
+                    case "scroll":
+
+                        break;
+                    case "search":
+
+                        break;
+                    case "return":
+                        driver.Navigate().GoToUrl("https://www.ubereats.com/pt-PT/feed/?d=2019-11-02&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkRFVEklMjAtJTIwRGVwYXJ0YW1lbnRvJTIwZGUlMjBFbGVjdHIlQzMlQjNuaWNhJTJDJTIwVGVsZWNvbXVuaWNhJUMzJUE3JUMzJUI1ZXMlMjBlJTIwSW5mb3JtJUMzJUExdGljYSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpzVjdhcjZxaUl3MFJidHRlelhxZVI3YyUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0E0MC42MzMxNzMxMDAwMDAwMSUyQyUyMmxvbmdpdHVkZSUyMiUzQS04LjY1OTQ5MzMlN0Q%3D&ps=1&st=1350");
+                        break;
+                }
+
+                switch ((string)json.recognized[2].ToString())
+                {
+
+                    case "MCDONALDS":
+                        //search mcdonalds
+                        var searchBox = driver.FindElement(By.CssSelector("input[class='bn ct']"));
+                        searchBox.SendKeys("mcdonalds universidade");
+                        searchBox.SendKeys(Keys.Enter);
+                        //pergunta ao user o que quer?
+                        break;
+                    case "MONTADITOS":
+
+                        break;
+                    case "PIZZAHUT":
+
+                        break;
+                }
+
+            }
+
+            /*
             switch ((string)json.recognized[2].ToString())
             {
 
                 case "MCDONALDS":
-
+                    //search mcdonalds
+                    //pergunta ao user o que quer?
                     break;
                 case "MONTADITOS":
-                    _s = circle;
+                    
                     break;
                 case "PIZZAHUT":
-                    _s = triangle;
+
                     break;
-            }
+            }*/
 
-            App.Current.Dispatcher.Invoke(() =>
-            {
-                switch ((string)json.recognized[1].ToString())
-                {
-                    case "GREEN":
-                        _s.Fill = Brushes.Green;
-                        break;
-                    case "BLUE":
-                        _s.Fill = Brushes.Blue;
-                        break;
-                    case "RED":
-                        _s.Fill = Brushes.Red;
-                        break;
-                }
-            });
-            
+        }
+
+        public static void openUberEatsChrome(ChromeDriver driver)
+        {
+            // Initialize the Chrome Driver
+            //using (driver)
+            //{
+            // 1. Maximize the browser
+            driver.Manage().Window.Maximize();
+
+            // 2. Go to the "Google" homepage
+            /*driver.Navigate().GoToUrl("https://www.ubereats.com/pt-PT/feed/?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkRFVEklMjAtJTIwRGVwYXJ0YW1lbnRvJTIwZGUlMjBFbGVjdHIlQzMlQjNuaWNhJTJDJTIwVGVsZWNvbXVuaWNhJUMzJUE3JUMzJUI1ZXMlMjBlJTIwSW5mb3JtJUMzJUExdGljYSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpzVjdhcjZxaUl3MFJidHRlelhxZVI3YyUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0E0MC42MzMxNzMxMDAwMDAwMSUyQyUyMmxvbmdpdHVkZSUyMiUzQS04LjY1OTQ5MzMlN0Q%3D");
+            */
+
+            driver.Navigate().GoToUrl("https://www.ubereats.com/pt-PT/feed/?d=2019-11-02&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkRFVEklMjAtJTIwRGVwYXJ0YW1lbnRvJTIwZGUlMjBFbGVjdHIlQzMlQjNuaWNhJTJDJTIwVGVsZWNvbXVuaWNhJUMzJUE3JUMzJUI1ZXMlMjBlJTIwSW5mb3JtJUMzJUExdGljYSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpzVjdhcjZxaUl3MFJidHRlelhxZVI3YyUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0E0MC42MzMxNzMxMDAwMDAwMSUyQyUyMmxvbmdpdHVkZSUyMiUzQS04LjY1OTQ5MzMlN0Q%3D&ps=1&st=1350");
+
+            // 3. Find the search textbox (by ID) on the homepage
 
 
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("button[class='ao aq b2']")));
+
+
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //driver.Manage().Window.Minimize();
+            //driver.Manage().Window.Maximize();
+            driver.FindElement(By.CssSelector("button[class='ao aq b2']")).Click(); //By.CssSelector("button[class='ao aq b2]'"));
+                                                                                    //driver.FindElementByCssSelector("button.ao.aq.b2");
+                                                                                    //searchBox.Click();
+                                                                                    //searchBox.SendKeys(Keys.Enter);
+                                                                                    //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            //wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("#search-suggestions-input")));
+
+
+
+
+
+
+            /*
+
+            new WebDriverWait(driver, MyDefaultTimeout).Until(
+            d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+
+
+            searchBox.SendKeys("DETI");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("location-enter-address-item-0")));
+
+            searchBox.SendKeys(Keys.Enter);
+            */
+
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            //wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("location-enter-address-item-0")));
+
+            //IWebElement deti = driver.FindElementByCssSelector("button[type='submit']");
+            //deti.Click();
+            // 6. Click "Submit" to start the search
+            //searchBox.SendKeys(Keys.Enter);
+
+            // 7. Find the "Id" of the "Div" containing results stats
+            //var searchResults = driver.FindElementById("resultStats");
+
+            //Console.ReadKey();
+            // }
         }
 
         public class GoogleSearchEngineUsingChrome
